1dfddbdb616b4ef3cecc9910528ab6285b7f6907
Ross Binnie <ross.binnie87@gmail.com>
2024-10-03 02:59:05 +0100

    Update merge-node-instruction.tsx

--- src/ui/merge-node-instruction.tsx
+++ src/ui/merge-node-instruction.tsx
@@ -439,7 +439,7 @@ export class MergedNodeInstruction {
                 start_y +
                 relayHeight / 2.0 -
                 SIZE / 4 +
-                LINKSPACING * (output_i - (outputs.length - 1) / 4)
+                LINKSPACING * (output_i - (outputs.length - 1) / 2)
 
             // Add output link
             const element = (
